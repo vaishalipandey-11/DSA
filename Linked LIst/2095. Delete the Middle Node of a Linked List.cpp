e5f154b25021d09efa779397bfcd64470e4ab0de
vaishali pandey <pandeyvaishali002@gmail.com>
2025-05-18 23:00:44 +0530

    learning

---
+++ Linked LIst/2095. Delete the Middle Node of a Linked List.cpp
@@ -0,0 +1,27 @@
+
+  struct ListNode {
+         int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(!head || !head -> next ) return  nullptr;
+        ListNode* slow = head ;
+         ListNode* fast = head->next->next;
+
+         while(fast && fast->next ){
+            slow = slow->next ;
+            fast = fast ->next->next;
+         }
+         ListNode* deleteit = slow->next ;
+         slow->next = slow->next->next;
+         delete(deleteit);
+         return head;
+    }
+};
