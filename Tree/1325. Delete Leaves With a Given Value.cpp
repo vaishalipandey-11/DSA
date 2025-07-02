13ba316570ad4852fb6cc3a2d12ca68a5dbf87f6
vp <pandeyvaishali002@gmail.com>
2025-07-02 20:34:11 +0530

    learning

---
+++ Tree/1325. Delete Leaves With a Given Value.cpp
@@ -0,0 +1,20 @@
+ struct TreeNode {
+      int val;
+    TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  };
+
+
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(!root) return nullptr;
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+
+        if(!root->left && !root->right && root->val == target) return NULL;
+
+        return root;
+    }
+};
--- Tree/1609. Even Odd Tree.cpp
+++ Tree/1609. Even Odd Tree.cpp
@@ -49,3 +49,41 @@ public:
 
     }
 };
+
+
+//Approach-2 (DFS)
+//T.C : O(n)
+//S.C : Auxiliary Space O(n) + Recursion Space O(depth)
+class Solution {
+public:
+    
+    vector<int> levelPrev;
+    
+    bool solve(TreeNode* root, int level) {
+        if(!root) {
+            return true;
+        }
+        
+        if((level%2 == 0 && root->val%2 == 0) || (level%2 != 0 && root->val%2 != 0)) {
+            return false;
+        }
+        
+        if(level >= levelPrev.size()) {
+            levelPrev.resize(level+1);
+        }
+
+        if(levelPrev[level] != 0) { //already there is a value at this level
+            if((level%2 == 0 && root->val <= levelPrev[level]) ||
+               (level%2 != 0 && root->val >= levelPrev[level]))
+                return false;
+        }
+        
+        levelPrev[level] = root->val;
+        
+        return solve(root->left, level+1) && solve(root->right, level+1);
+    }
+    
+    bool isEvenOddTree(TreeNode* root) {
+        return solve(root, 0);
+    }
+};
