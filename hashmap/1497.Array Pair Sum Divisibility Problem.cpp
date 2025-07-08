b04cb8a88d122f80c5a3a0098ffe82b1a3d6540d
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-07-08 10:38:24 +0530

    learning

---
+++ hashmap/1497.Array Pair Sum Divisibility Problem.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+using namespace std;
+class Solution {
+  public:
+    bool canPair(vector<int> &arr, int k) {
+       int n = arr.size();
+       
+       if(n%2 !=0 ) return false;
+       
+       vector<bool>used(n,false);
+       
+       for(int i=0 ; i<n ;i++){
+           if(used[i]) continue;
+           
+           bool foundpair = false;
+           
+           for(int j =i+1 ; j<n ; j++){
+               
+               if(!used[j] && (arr[i]+arr[j])%k ==0 ) {
+                   
+                   foundpair = true;
+                   used[i]=used[j]=true;
+                   break;
+               }
+           }
+           if(!foundpair) return false;
+       }
+       
+       return true;
+    }
+};
+
+
+
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+
+        int n = arr.size();
+        if (n % 2 != 0) return false; // can't pair an odd-sized array
+
+        unordered_multiset<int> remainder;
+
+        for (int i = 0; i < n; i++) { // find remainder and complement
+            int rem = ((arr[i] % k) + k) % k;
+            int complement = (k - rem) % k;
+
+            if (remainder.count(complement) > 0) {  // if complement exists
+                remainder.erase(remainder.find(complement));
+            } else {
+                remainder.insert(rem);  // insert rem, not complement
+            }
+        }
+        return remainder.empty(); 
+    }
+};
