1520b96759a3b5ac0a8947ef4a685120d7070edf
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-07-14 22:28:21 +0530

    learning

--- DP/ Code Testcase Testcase Test Result 1911. Maximum Alternating Subsequence Sum.cpp
+++ DP/ Code Testcase Testcase Test Result 1911. Maximum Alternating Subsequence Sum.cpp
@@ -59,3 +59,19 @@ public:
     }
 };
 
+
+class Solution {
+public:
+int n;
+    long long maxAlternatingSum(vector<int>& nums) {
+        n = nums.size();
+        vector<vector<long >> dp(n+1 , vector<long>(2,0));
+        for(int  i=1 ; i<n+1 ; i++){
+            // even krne se odd ho gaya
+            dp[i][0] =max(dp[i-1][1]-nums[i-1] , dp[i-1][0]);
+            //if even 
+            dp[i][1] = max(dp[i-1][0]+nums[i-1], dp[i-1][1]);
+        }
+        return max(dp[n][0],dp[n][1]);        
+    }
+};
