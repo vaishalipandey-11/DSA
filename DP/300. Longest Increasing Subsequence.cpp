6bea73109eaf4f6be06ecf311082c32a21d8fb9a
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-07-15 09:09:20 +0530

    learning

--- DP/300. Longest Increasing Subsequence.cpp
+++ DP/300. Longest Increasing Subsequence.cpp
@@ -53,3 +53,21 @@ public:
        return solve(nums,0,-1);
     }
 };
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        int maxLIS = 1;
+        vector<int>dp(n , 1); // init with 1 qki sb hikhud k subseq h
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<i ; j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = max(dp[i], dp[j]+1);
+                    maxLIS = max(maxLIS , dp[i]);
+                }
+            }
+        }
+        return maxLIS;
+    }
+};
