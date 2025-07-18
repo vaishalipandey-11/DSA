3b82117bb3a5571ceec2562d2363fd4be72cb757
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-07-18 18:29:42 +0530

    learning

--- DP/72. Edit Distance.cpp
+++ DP/72. Edit Distance.cpp
@@ -65,3 +65,58 @@ public:
         
     }
 };
+//this is starting from m and n 
+
+class Solution {
+public:
+    int n , m;
+    int dp[501][501];
+    int solve(string &word1, string &word2 , int m , int n){
+        if(m==0 || n ==0) return m+n;
+        if(dp[m][n] != -1 ) return dp[m][n];
+
+        if(word1[m-1]==word2[n-1]){
+          return   solve(word1, word2 ,m-1, n-1);
+        }else{
+            int insertc = 1+ solve(word1 , word2 ,m , n-1);
+            int deletec = 1+ solve(word1 ,word2, m-1 , n);
+            int replacec = 1+ solve(word1 , word2 ,m-1 , n-1 );
+            return dp[m][n] = min(insertc , min(deletec , replacec));
+        }
+        return -1;
+        
+    }
+    int minDistance(string word1, string word2) {
+         m= word1.size();
+         n = word2.size();
+         memset(dp , -1 , sizeof(dp));
+        return solve(word1, word2 , m,n);
+        
+    }
+};
+
+//this is bottom up approach
+//tc : O(m*n)
+//sc : O(m*n)   
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+            for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = i + j;
+                else if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else {
+                    dp[i][j] = 1+
+                        min({dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]});
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
