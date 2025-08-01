c49ab31b5eecb16cba6206718ec4054babc2a765
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-08-01 23:25:18 +0530

    learning

--- Arrays/118. Pascal's Triangle.cpp
+++ Arrays/118. Pascal's Triangle.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>ans(numRows);
         for(int i =0 ; i<numRows ; i++){
             ans[i] = vector<int>(i+1 , 1);
-            for(int j =1 ; j<i ; j++){
+            for(int j =1 ; j<i ; j++){ // start from 1 to avoid the first and last element which are always 1
                 ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
             }
         }
