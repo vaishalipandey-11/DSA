34b34c4e39997afb1eb5d6251ad136ff7abee289
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-08-07 23:44:22 +0530

    solving arrays

---
+++ Arrays/498. Diagonal Traverse.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+      int n = mat.size();
+      if(n==0) return {};
+      int m = mat[0].size();
+
+      vector<int>result;
+      map<int,vector<int>>mp;
+
+      for(int i =0 ; i <n ; i++){
+        for(int j =0 ; j<m ; j++){
+            mp[i+j].push_back(mat[i][j]);
+        }
+      }
+
+      bool flag = true;
+      for(auto &it : mp){
+       if(flag){
+        reverse(it.second.begin(), it.second.end());
+       }
+
+       for(auto &val : it.second){
+         result.push_back(val);
+       }
+
+        flag = !flag;
+      }
+      return result ;
+    }
+};
