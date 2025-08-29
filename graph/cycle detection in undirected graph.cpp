31cff21407c44c701ba1e6b045d884892060507c
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-08-29 23:21:01 +0530

    learning  cycle detection in undirected graph using bfs

--- graph/cycle detection in undirected graph.cpp
+++ graph/cycle detection in undirected graph.cpp
@@ -43,3 +43,60 @@ class Solution {
              
         }
 };
+
+
+//now lets try  using bfs 
+class Solution {
+  public:
+  bool iscyclebfs(int u , vector<bool>&vis, unordered_map<int , vector<int>>&adj){
+      queue<pair<int,int>>q;
+      q.push({u , -1});
+      vis[u]=true;
+      
+      while(!q.empty()){
+          int s = q.size();
+          while(s--){
+              pair<int,int>p = q.front();
+              q.pop();
+              
+              int source = p.first;
+              int parent = p.second;
+              
+              
+              for(auto & v : adj[source]){
+                  if(vis[v]== false){
+                      vis[v]=true;
+                      q.push({v, source});
+                  }else if(v != parent ){
+                      return true ;
+                  }
+                  
+              }
+          }
+      } 
+      return false;
+      
+  }
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
+        unordered_map<int , vector<int>>adj;
+        for(auto &i : edges){
+            int u = i[0];
+            int v = i[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<bool>vis(V,false);
+        
+        for(int  i=0 ; i<V ; i++ ){
+            if(!vis[i] && iscyclebfs(i , vis , adj )){
+                return true;
+            }
+        }
+        return false;
+        
+        
+        
+    }
+};
