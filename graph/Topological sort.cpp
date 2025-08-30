2847917f21b22c89c7afdb6ba8e669b758404fa8
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-08-30 10:17:20 +0530

    topological sort  in graph using bfs

--- graph/Topological sort.cpp
+++ graph/Topological sort.cpp
@@ -43,3 +43,41 @@ class Solution {
 };
 
 //topological sort using bfs called khan's algorithm
+
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        
+         vector<int>indegree(V,0);
+        
+        unordered_map<int , vector<int>>adj;
+        for(auto & i : edges){
+            int u= i[0];
+            int v = i[1];
+            adj[u].push_back(v);
+              indegree[i[1]]++;
+        }
+        
+        queue<int>q;
+        vector<int >res;
+        
+        for(int i=0 ; i<V ; i++){
+            if(indegree[i]==0)
+            q.push(i);
+        }
+        
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            res.push_back(u);
+            
+            for(auto &v: adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                   q.push(v);
+                }
+            }
+        } 
+        return res;
+    }
+};
