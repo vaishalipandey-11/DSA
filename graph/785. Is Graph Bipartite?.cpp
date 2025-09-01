504bf90dad305a2f030842e3a48e5a7e6f6cf75b
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-09-01 10:35:06 +0530

    solving bipartite graph using bfs

--- graph/785. Is Graph Bipartite?.cpp
+++ graph/785. Is Graph Bipartite?.cpp
@@ -40,3 +40,49 @@ public:
         
     }
 };
+
+// using bfs as well
+class Solution {
+public:
+    bool checkbfs(unordered_map<int, vector<int>>& adj, vector<int>& clr,
+                  int currnode, int currclr, queue<int>& q) {
+        clr[currnode] = currclr;
+        q.push(currnode);
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (auto& v : adj[u]) {
+                if (clr[v] == currclr)
+                    return false;
+                if (clr[v] == -1) {
+                    clr[v] = 1 - currclr;
+
+                    if (checkbfs(adj, clr, v, 1 - currclr,q) == false)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        unordered_map<int, vector<int>> adj;
+       
+        for (int i = 0; i < V; i++) {
+            adj[i] = graph[i];
+        }
+
+        vector<int> clr(V, -1);
+        queue<int> q;
+
+        for (int i = 0; i < V; i++) {
+            if (clr[i] == -1) {
+                if (checkbfs(adj, clr, i, 0,q) == false)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
