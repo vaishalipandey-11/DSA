a73e9d9c2f944818b488f92634265d3ebda607c0
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-09-02 20:16:40 +0530

    path and rank compression  DSU(disjoint set union)

--- graph/DSU(disjoint set union ).cpp
+++ graph/DSU(disjoint set union ).cpp
@@ -15,12 +15,24 @@
         }
        
     }
+    // PATH COMPRESSION AND RANK COMPRESSION
 
-    // PATH COMPRESSION 
-    int findd(int i , vector<int>parent){
-        if(i==parent[i]) return i;
-        return parent[i]= findd(parent[i] , parent);
+   int findd(int i, vector<int> &parent) {
+    if (i == parent[i]) return i;
+    return parent[i] = findd(parent[i], parent); // path compression
+}
 
+void unionn(vector<int> &parent, int i, int j, vector<int> &rank) {
+    int i_parent = findd(i, parent);
+    int j_parent = findd(j, parent);
+    if (i_parent == j_parent) return;
+
+    if (rank[i_parent] == rank[j_parent]) {
+        parent[i_parent] = j_parent;
+        rank[j_parent]++;
+    } else if (rank[i_parent] < rank[j_parent]) {
+        parent[i_parent] = j_parent;
+    } else {
+        parent[j_parent] = i_parent;
     }
-
-    //union compression dsu
+}
