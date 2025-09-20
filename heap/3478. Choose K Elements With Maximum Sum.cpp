70159a59d6701a6c403e3c2eb2ddb8bf6417b467
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-09-20 23:13:56 +0530

    heap

--- heap/3478. Choose K Elements With Maximum Sum.cpp
+++ heap/3478. Choose K Elements With Maximum Sum.cpp
@@ -27,3 +27,47 @@ public:
         return v;
     }
 };
+
+
+
+
+class Solution {
+public:
+typedef long long ll;
+
+    vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+        vector<vector<int>> v;
+        for(int i=0; i<n ; i++){
+            v.push_back({nums1[i], i , nums2[i]});
+        }
+
+        sort(v.begin(), v.end());
+
+        vector<ll >result(n,0);
+
+        priority_queue<int , vector<int> , greater<int>> p;
+        ll sum =0;
+        for(int i=0 ; i<n ; i++){
+           
+            if(i>0 &&  v[i-1][0]== v[i][0]){
+                result[v[i][1]]=result[v[i-1][1]];
+            }else{
+                result[v[i][1]]=sum ;
+            }
+
+            p.push(v[i][2]);
+            sum += v[i][2];
+            if(p.size() > k){
+                sum -= p.top();
+                p.pop();
+            }
+
+        }
+        return result ;
+
+
+
+        
+    }
+};
