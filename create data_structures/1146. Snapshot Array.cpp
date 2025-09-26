520e0b840ea0c808e6e369a81ddc84795632d664
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-09-26 09:16:06 +0530

    design data structure

---
+++ create data_structures/1146. Snapshot Array.cpp
@@ -0,0 +1,74 @@
+
+ #include<iostream>
+ #include<bits/stdc++.h>
+ using namespace std;
+class SnapshotArray {
+public:
+int snap_id ;
+   
+    vector<vector<pair<int,int>>> vec;
+    SnapshotArray(int length) {
+        snap_id =0;
+        vec.resize(length);
+
+        for(int i=0; i<length ; i++){
+            vec[i].push_back(make_pair(0,0));
+        }
+    }
+    
+    void set(int index, int val) {
+        vec[index].push_back(make_pair(snap_id , val));
+        
+    }
+    
+    int snap() {
+      return snap_id ++;
+        
+    }
+    
+    int get(int index, int snap_id) {
+       int l =0 ; int r= vec[index].size()-1; int result =0;
+       while ( l<=r ) {
+        int mid = l+(r-l)/2;
+        if(vec[index][mid].first <= snap_id){
+            result = vec[index][mid].second;
+            l =mid+1;
+        }else{
+            r=mid-1;
+        }
+       }
+       return result ;
+        
+    }
+};
+
+class SnapshotArray {
+public:
+int snap_id ;
+    vector<vector<pair<int,int>>> vec;
+    SnapshotArray(int length) {
+        snap_id =0;
+        vec.resize(length);
+
+        for(int i=0; i<length ; i++){
+            vec[i].push_back(make_pair(0,0));
+        }
+    }
+    
+    void set(int index, int val) {
+        vec[index].push_back(make_pair(snap_id , val));
+        
+    }
+    
+    int snap() {
+      return snap_id ++;
+        
+    }
+    
+    int get(int index, int snap_id) {
+        auto it = upper_bound(vec[index].begin(), vec[index].end(), make_pair(snap_id , INT_MAX));
+        
+        return prev(it)->second;
+        
+    }
+};
