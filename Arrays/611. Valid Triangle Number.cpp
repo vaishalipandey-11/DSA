554a556c94b8184a7a241130ad29af1f23cb26e1
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-10-01 00:59:08 +0530

    cd  inti|| kill

---
+++ Arrays/611. Valid Triangle Number.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans =0;
+        for(int i=0 ; i<n-2 ; i++){
+            for(int j = i+1 ; j<n-1 ; j++){
+                for(int k= j+1 ; k<n ; k++ ){
+                    if(nums[i]+nums[j]>nums[k]){
+                        ans++;
+                    }
+
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
