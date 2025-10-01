72fe90e088bf0d0fd836d5a8952f7500970b2647
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-10-01 22:29:22 +0530

    ptod

--- Mathematics/1518. Water Bottles.cpp
+++ Mathematics/1518. Water Bottles.cpp
@@ -5,3 +5,18 @@ public:
     }
 };
 
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int result = numBottles; 
+        int empty = numBottles;
+
+        while (empty >= numExchange) {
+            int newBottles = empty / numExchange;  
+            result += newBottles;                 
+            empty = empty % numExchange + newBottles;
+        }
+
+        return result;
+    }
+};
