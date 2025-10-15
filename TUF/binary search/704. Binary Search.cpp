02f2fd54104328d605f47ce66c5b6006f9b9893c
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-10-15 12:23:20 +0530

    bs learning

--- TUF/binary search/704. Binary Search.cpp
+++ TUF/binary search/704. Binary Search.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -16,5 +20,26 @@ public:
             }
         }
         return -1;
+    }
+};
+
+
+class Solution {
+public:
+    int bs(vector<int>& nums , int low , int high , int target){
+        if(low > high ) return -1; // bc
+        int mid = (low+high)/2;
+
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]< target){
+           return  bs(nums , mid+1 , high, target );
+        }
+        else
+          return   bs(nums, low , mid-1 , target);
+    }
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;  int high = n-1;
+       return bs(nums,low , high, target);
     }
 };
