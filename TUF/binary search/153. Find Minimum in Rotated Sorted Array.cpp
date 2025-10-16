f34970a926e35e4dafb5a4004ddb8a2e70670d8a
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-10-16 09:22:26 +0530

    learning

--- TUF/binary search/153. Find Minimum in Rotated Sorted Array.cpp
+++ TUF/binary search/153. Find Minimum in Rotated Sorted Array.cpp
@@ -23,3 +23,34 @@ public:
         return ans;
     }
 };
+
+
+//further optimization technique
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0;
+        int h = n - 1;
+        int ans = INT_MAX;
+
+        while (l <= h) {
+
+            int mid = l + (h - l) / 2;
+            // if its already sorted then we need to return the minimum element which is low
+            if(nums[l]<= nums[h]){
+                ans= min(ans, nums[l]);
+                break;
+            }
+
+            if(nums[l] <= nums[mid]){
+                ans = min(ans, nums[l]);
+                l=mid+1;
+            }else{
+                h=mid;
+
+            }
+        }
+        return ans;
+    }
+};
