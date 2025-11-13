0214bccd5f3e3542f73919015d7e2277e7ebd7d8
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-11-13 12:39:32 +0530

    strings

--- TUF/String/151. Reverse Words in a String.cpp
+++ TUF/String/151. Reverse Words in a String.cpp
@@ -30,3 +30,38 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        string temp = "";
+        string ans = "";
+
+        while (left <= right) {
+            char ch = s[left];
+            if (ch == ' ') {
+                if (!temp.empty()) {
+                    if (!ans.empty()) ans = temp + " " + ans;
+                    else ans = temp;
+                    temp = "";
+                }
+            } else {
+                temp += ch;
+            }
+            left++;
+        }
+
+        // last word
+        if (!temp.empty()) {
+            if (!ans.empty()) {
+                ans = temp + " " + ans;
+            } else {
+                ans = temp;
+            }
+        }
+        return ans;
+    }
+};
