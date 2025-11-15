309ec37a98f50ce7f73c3d9f7687a08204795463
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-11-15 23:24:19 +0530

    ..

---
+++ TUF/String/14. Longest Common Prefix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        string sol ;
+        sort(strs.begin(), strs.end());
+        for(int i=0 ; i<strs[0].size() ; i++){
+        char curr_ch = strs[0][i];
+        
+
+        for(int j = 1 ; j<strs.size() ; j++){
+            if(i>=strs[j].size() || strs[j][i] != curr_ch){
+                return sol;
+            }
+        }
+        
+       sol.push_back(curr_ch);
+        }
+        return sol;
+        
+    }
+};
+
