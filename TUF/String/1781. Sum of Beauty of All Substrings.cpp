1e0c95bb27cdece0ee2b65c5a7819466faeb98d0
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2025-11-18 23:59:46 +0530

    str

---
+++ TUF/String/1781. Sum of Beauty of All Substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.length();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+
+            unordered_map<char, int> freq;
+
+            for (int j = i; j < n; j++) {
+                int MINI = INT_MAX;
+                int MAXI = INT_MIN;
+
+                freq[s[j]]++;
+
+                for (auto& it : freq) {
+                    MINI = min(MINI, it.second);
+                    MAXI = max(MAXI, it.second);
+                }
+
+                sum += MAXI - MINI;
+            }
+        }
+        return sum;
+    }
+};
